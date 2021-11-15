2abf8f0274cf205c0fb1c96b97ed268df97ece26
thuliobandeira <thuliao18@gmail.com>
2021-11-14 22:26:38 -0300

    editei as tabelas e consertei os bugs

--- script.js
+++ script.js
@@ -1,21 +1,10 @@
-function add(a,b){
-    return a + b;
-}
-function multiply(a,b){
-    let soma = 0;
-    for(let i =1; i<b;i++){
-        soma=add(soma,a)
-    }
-    return soma;
-}
-
 function tabelaMultiplicação() { 
     // TODO: escrever dois loops aninhados para desenhar o array bidimensional e monstar o restante da tabela no console.
     let x = [];
-    for (let i = 1; i < 12; i++) {
+    for (let i = 0; i <= 10; i++) {
         x[i] = [];
-        for(let c = 0; c<11; c++){
-            x[i].push(multiply(c,i))
+        for(let c = 0; c<=10; c++){
+            x[i][c] = i*c
 
         }
     }
@@ -24,10 +13,10 @@ function tabelaMultiplicação() {
 function tabelaMultiplicaçãoBonus(num) { 
     // TODO: escrever dois loops aninhados para desenhar o array bidimensional e monstar o restante da tabela no console.
     let x = [];
-    for (let i = 0; i <= 10; i++) {
+    for (let i = 0; i <= num; i++) {
         x[i] = [];
         for(let c = 0; c<=num; c++){
-            x[i].push(multiply(c,i))
+            x[i][c] = i*c
 
         }
     }
